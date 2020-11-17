cdc3b52b8e401587691df3a9b3e673bdc0129721
Miguel Saliba <miguosaliba@gmail.com>
2020-11-17 19:12:05 +0300

    slight improvements

--- bot/main.py
+++ bot/main.py
@@ -2,25 +2,27 @@ import discord
 from discord.ext import commands
 import os
 
-client = commands.Bot(command_prefix=".")
+client = commands.Bot(command_prefix="m.")
 token = os.getenv("DISCORD_BOT_TOKEN")
+admins = ["244876633528336384"]
 
 @client.event
 async def on_ready() :
-    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+    await client.change_presence(activity = discord.Game("with kids"))
     print("I am online")
 
 @client.command()
 async def ping(ctx) :
-    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+    await ctx.send(f"🏓 Pong with {str(round(client.latency * 1000))}ms")
 
-@client.command(name="whoami")
+@client.command()
 async def whoami(ctx) :
     await ctx.send(f"You are {ctx.message.author.name}")
 
 @client.command()
 async def clear(ctx, amount=3) :
-    await ctx.channel.purge(limit=amount)
+    if (ctx.message.author.id in admins):
+        await ctx.channel.purge(limit=amount)
 
 
 client.run(token)
